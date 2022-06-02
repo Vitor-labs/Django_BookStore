596b506f93285db529db964cb51f971f3b3455d1
Vitor Hugo <v02hx10@gmail.com>
2022-06-01 21:01:11 -0300

    ADD: store models, serials

--- store/models.py
+++ store/models.py
@@ -1,3 +1,83 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    date_added = models.DateField(auto_now_add=True)
+
+    client = models.OneToOneField(
+        'store.Client',
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    items = models.ManyToManyField(
+        'inventory.Book',
+        related_name='carts',
+    )
+
+    class Meta:
+        db_table = 'Cart'
+        ordering = ['date_added']
+
+    def __str__(self):
+        return self.cart_id
+
+
+class Client(models.Model):
+    client_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(
+        'auth.User',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    state = models.CharField(max_length=255, blank=True, null=True)
+    zip_code = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+
+    credit_card_number = models.CharField(
+        max_length=255, blank=True, null=True)
+    credit_card_expiration_date = models.CharField(
+        max_length=255, blank=True, null=True)
+    credit_card_cvv = models.CharField(
+        max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'Client'
+
+    def __str__(self):
+        return self.client_id
+
+
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    date_added = models.DateField(auto_now_add=True)
+    payment_method = models.CharField(
+        max_length=255, blank=True, null=True)
+
+    client = models.ForeignKey(
+        'store.Client',
+        on_delete=models.CASCADE,
+    )
+    cart = models.ForeignKey(
+        'store.Cart',
+        on_delete=models.CASCADE,
+    )
+    amount = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+
+    class Meta:
+        db_table = 'Payment'
+        ordering = ['date_added']
+
+    def __str__(self):
+        return self.payment_id
---
+++ store/serializer.py
@@ -0,0 +1,31 @@
+from attr import fields
+from rest_framework import serializers
+
+from store.models import Cart, Client, Payment
+
+
+class CartSerializer(serializers.HyperlinkedModelSerializer):
+    class meta:
+        model = Cart
+        fields = ['__all__']
+
+
+class ClientSerializer(serializers.HyperlinkedModelSerializer):
+    class meta:
+        model = Client
+        fields = ['__all__']
+
+    def create(self, validated_data):
+        ...
+        return Client.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        ...
+        instance.save()
+        return instance
+
+
+class PaymentSerializer(serializers.HyperlinkedModelSerializer):
+    class meta:
+        model = Payment
+        fields = ['__all__']
