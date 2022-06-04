deaf73591321735355a01e4139b5600f3759358d
Vitor Hugo <v02hx10@gmail.com>
2022-06-03 23:43:39 -0300

    MOD: Testing logic

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,49 +1,41 @@
 from django.test import TestCase
+from faker import Faker
 
 from inventory.models import Book
 
+fake = Faker()
+
+
 class BookTestCase(TestCase):
     def setUp(self):
-        self.book = Book.objects.create(title="Test Book", 
-                                        author="Test Author", 
-                                        summary="Test Summary", 
-                                        pages=1, rating=0.0, price=1.0, 
-                                        isbn="123456789", 
-                                        publisher="Test Publisher", 
-                                        pub_date="2017-01-01", 
-                                        cover="Test Cover",
-                                        genre="Test Genre")
+        self.book = Book.objects.create(
+            title=fake.name(),
+            author=fake.name(),
+            summary=fake.text(),
+            pages=fake.random_int(min=1, max=1500),
+            rating=fake.random_int(min=1, max=5),
+            price=fake.random_number(digits=4),
+            isbn=fake.isbn10(),
+            publisher=fake.company(),
+            pub_date=fake.date_between(start_date="-10y", end_date="today"),
+            cover=fake.image_url(width=None, height=None),
+            genre=fake.name()
+        )
 
-    def test_book_title(self):
-        self.assertEqual(self.book.title, "Test Book")
+    def test_book_creation(self):
+        self.assertTrue(isinstance(self.book, Book))
+        self.assertEqual(self.book.__str__(), self.book.title)
+    
+    def test_book_list(self):
+        books = Book.objects.all()
+        self.assertTrue(len(books) > 0)
 
-    def test_book_author(self):
-        self.assertEqual(self.book.author, "Test Author")
+    def test_book_update(self):
+        self.book.title = fake.name()
+        self.book.save()
+        self.assertEqual(self.book.title, self.book.__str__())
 
-    def test_book_summary(self):
-        self.assertEqual(self.book.summary, "Test Summary")
-
-    def test_book_pages(self):
-        self.assertEqual(self.book.pages, 1)
-
-    def test_book_rating(self):
-        self.assertEqual(self.book.rating, 0.0)
-
-    def test_book_price(self):
-        self.assertEqual(self.book.price, 1.0)
-
-    def test_book_isbn(self):
-        self.assertEqual(self.book.isbn, "123456789")
-
-    def test_book_publisher(self):
-        self.assertEqual(self.book.publisher, "Test Publisher")
-
-    def test_book_pub_date(self):
-        self.assertEqual(self.book.pub_date, "2017-01-01")
-
-    def test_book_cover(self):
-        self.assertEqual(self.book.cover, "Test Cover")
-
-    def test_book_genre(self):
-        self.assertEqual(self.book.genre, "Test Genre")
+    def test_book_delete(self):
+        self.book.delete()
+        self.assertFalse(Book.objects.filter(id=self.book.id).exists())
 
