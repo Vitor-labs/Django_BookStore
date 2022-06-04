b6c52c7ef551a6e3b4c6bf095982100441937294
Vitor Hugo <v02hx10@gmail.com>
2022-06-04 14:25:59 -0300

    ADD: more auth options

--- Book_Store/settings.py
+++ Book_Store/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
