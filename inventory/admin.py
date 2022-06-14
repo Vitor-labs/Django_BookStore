13753f798cf7acce11f1c706edf5776d165c28f5
Vitor Hugo <v02hx10@gmail.com>
2022-06-13 23:16:01 -0300

    ADD: books admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,25 @@
+# Django Libs:
 from django.contrib import admin
+# Local Libs:
+from .models import Book
 
-# Register your models here.
+
+class CustomBook(admin.ModelAdmin):
+	"""Allow to edit Book informations"""
+	# list_display = ('', )
+	fieldsets = [
+		('title', {'fields': ['title']}),
+		('author', {'fields': ['author']}),
+		('summary', {'fields': ['summary']}),
+		('pages', {'fields': ['pages']}),
+		('rating', {'fields': ['rating']}),
+		('price', {'fields': ['price']}),
+		('isbn', {'fields': ['isbn']}),
+		('publisher', {'fields': ['publisher']}),
+		('pub_date', {'fields': ['pub_date']}),
+		('cover', {'fields': ['cover']}),
+		('genre', {'fields': ['genre']}),
+	]
+
+
+admin.site.register(Book, CustomBook)
